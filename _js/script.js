742f75e1f56a28786c823553fb32ff404dfefab5
Sandra Helsley <syh@ischool.berkeley.edu>
2013-09-28 18:29:16 -0700

    Adding styles and basic structure.

--- _js/script.js
+++ _js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
+  $('#trailFinder').on('submit', function(){
+    var username, password, playlist;
 
-}
+    username = $('#targetUser').val();
+    password = $('#password').val();
+    playlist = $('#playlist').val();
+
+    return false;
+  });
+
+})
