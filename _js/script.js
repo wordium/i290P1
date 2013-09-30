7d710b0058162c31ac68cfa2ab843f8c290a0bfe
tomnar <tomquast89@gmail.com>
2013-09-29 18:18:04 -0700

    setting up the search request for a specific playlist

--- _js/script.js
+++ _js/script.js
@@ -1,3 +1,6 @@
+var KEY = "AIzaSyDZeRhva5hPAP7JHzk6-iD6wZhCKj1guaU"; //My private key USE WITH CAREFULLY AND DONT DISTRIBUTE
+var BASE = "https://www.googleapis.com/youtube/v3"; //We use version three of the google API
+
 $(document).ready(function() {
   $('#trailFinder').on('submit', function(e){
 	e.preventDefault(); //atm let's not reload the page
@@ -13,12 +16,10 @@ $(document).ready(function() {
 	var part = "snippet"; //include more information e.g. preview pictures
 	var order = "viewCount"; //ordering (date, rating, relevance, title, videoCount or viewCount)
 	var type = "playlist"; //type of videos (channel, playlist or video)
-	var key = "AIzaSyDZeRhva5hPAP7JHzk6-iD6wZhCKj1guaU"; //My private key USE WITH CAREFULLY AND DONT DISTRIBUTE
 	var maxResults = 20; //Max. results to fetch [1-50]
 	var method = "search" //We will only be searching content
 	var q = "cats"; //Query string !!NEEDS TO BE ALTERED SO IT ACCEPTS DATA FROM THE USER!!
-	var baseUrl = "https://www.googleapis.com/youtube/v3"; //We use version three of the google API
-	var fullUrl = baseUrl + "/" + method + "?part=" + part + "&order=" + order + "&type=" + type + "&key=" + key + "&maxResults=" + maxResults + "&q=" + q;
+	var fullUrl = BASE + "/" + method + "?part=" + part + "&order=" + order + "&type=" + type + "&key=" + KEY + "&maxResults=" + maxResults + "&q=" + q;
 	
 	//Make an async call to youtubes API v.3
 	$.ajax({
@@ -28,12 +29,31 @@ $(document).ready(function() {
 		error: error,
 		dataType: "json"
 	});
+	
+	
+	//One of the playlists has been clicked, preview all videos in it
+	// !!NEEDS TO BE CALLED FROM THE GUI, MAYBE BY CLICKING ON FX. A PREVIEW PIC!!
+	/*Performing youtube-playlist search*/
+	var method = "playlistItems"; //We will want to search in a specific playlist
+	var playlistId = "PL26112E48392C500F" //The Id of the playlist we want to search in !!NEEDS TO BE ALTERED SO IT ACCEPTS DATA FROM THE USER!!
+	var part = "snippet" //include more information e.g. preview pictures
+	var maxResults = 50; //Max. results to fetch [1-50]
+	var fullUrl = BASE + "/" + method + "?playlistId=" + playlistId + "&part=" + part + "&key=" + KEY + "&maxResults=" + maxResults;
+	
+	//Make an async call to youtubes API v.3
+	$.ajax({
+		type: "GET",
+		url: fullUrl,
+		success: parseYoutubePLJSON,
+		error: error,
+		dataType: "json"
+	});
   });
 })
 
 //Youtube-search callback
 function parseYoutubeJSON(data){
-	console.log(data);
+	//console.log(data);
 	var items = data.items;
 	for (var key in items){
 		var list = items[key];
@@ -46,9 +66,15 @@ function parseYoutubeJSON(data){
 		
 		//Check if all data is correct 
 		//console.log("id: " + id + "cTitle: " + cTitle + "title: " + title + "description: " + description + "date: " + date + "image: " + image)
+		//http://www.youtube.com/embed/videoseries?list=PL9C5815B418D1508E&index=1
+		
 	}
 	
-	//http://www.youtube.com/embed/videoseries?list=PL9C5815B418D1508E&index=1
+}
+
+//Youtube-playlist-search callback
+function parseYoutubePLJSON(data){
+	console.log(data);
 }
 
 function error(e){
